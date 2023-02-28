baa051826ab9bc4bbc95fb395739f1c7c4b00cf8
Nayara <nayaranasariasoares@gmail.com>
2023-02-28 20:59:08 -0300

    Dark Mode

--- trocartema.js
+++ trocartema.js
@@ -1,14 +1,5 @@
-const button = document.querySelector(".js-theme");
+const chk = document.getElementById("chk");
 
-const themes = {
-  "t-dark": "t-light",
-  "t-light": "t-dark",
-};
-
-if (button) {
-  button.addEventListener("click", (event) => {
-    event.preventDefault();
-    const currentTheme = document.body.dataset.theme;
-    document.body.setAttribute("data-theme", themes[currentTheme] || "t-light");
-  });
-}
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+})
