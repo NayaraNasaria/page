0361c7c8769e167dc9027a37d0c606019796bfd2
Nayara Nasaria Soares <nayaranasariasoares@gmail.com>
2023-03-12 22:56:57 -0300

    Soft Skills add

--- js/script.js
+++ js/script.js
@@ -1,63 +1,64 @@
 const php = document.getElementById("php");
 const mysql = document.getElementById("mysql");
 
-const python = document.getElementById("python");
 const js = document.getElementById("js");
 const html = document.getElementById("html");
 const css = document.getElementById("css");
 const java = document.getElementById("java");
-
-const setaRight = document.getElementById("right");
-const setaLeft = document.getElementById("left");
 
 const one = document.getElementById("1");
 const two = document.getElementById("2");
 const three = document.getElementById("3");
 const four = document.getElementById("4");
 const five = document.getElementById("5");
+const six = document.getElementById("6");
 
-setaLeft.addEventListener("click", () => {
-  php.style.left = "120px";
+one.addEventListener("click", () => {
+  php.style.left = "80px";
+  js.style.left = "500px";
+  html.style.left = "-150px";
+  css.style.left = "-150px";
+  java.style.left = "-150px";
+  mysql.style.left = "-150px";
+});
+two.addEventListener("click", () => {
+  php.style.left = "-150px";
+  js.style.left = "80px";
+  html.style.left = "-150px";
+  css.style.left = "-150px";
+  java.style.left = "-150px";
+  mysql.style.left = "-150px";
+});
+three.addEventListener("click", () => {
+  php.style.left = "-150px";
+  js.style.left = "500px";
+  html.style.left = "80px";
+  css.style.left = "-150px";
+  java.style.left = "-150px";
+  mysql.style.left = "-150px";
+});
+four.addEventListener("click", () => {
+  php.style.left = "-150px";
+  js.style.left = "500px";
+  html.style.left = "-150px";
+  css.style.left = "80px";
+  java.style.left = "-150px";
+  mysql.style.left = "-150px";
+});
+five.addEventListener("click", () => {
+  php.style.left = "-150px";
+  js.style.left = "500px";
+  html.style.left = "-150px";
+  css.style.left = "-150px";
+  java.style.left = "80px";
   mysql.style.left = "-150px";
 });
 
-setaRight.addEventListener("click", () => {
+six.addEventListener("click", () => {
+  mysql.style.left = "80px";
   php.style.left = "-150px";
-  mysql.style.left = "120px";
-});
-
-one.addEventListener("click", () => {
-  python.style.left = "120px";
-  js.style.left = "-150px";
+  js.style.left = "500px";
   html.style.left = "-150px";
   css.style.left = "-150px";
   java.style.left = "-150px";
-});
-two.addEventListener("click", () => {
-  python.style.left = "-150px";
-  js.style.left = "120px";
-  html.style.left = "-150px";
-  css.style.left = "-150px";
-  java.style.left = "-150px";
-});
-three.addEventListener("click", () => {
-  python.style.left = "-150px";
-  js.style.left = "-150px";
-  html.style.left = "120px";
-  css.style.left = "-150px";
-  java.style.left = "-150px";
-});
-four.addEventListener("click", () => {
-  python.style.left = "-150px";
-  js.style.left = "-150px";
-  html.style.left = "-150px";
-  css.style.left = "120px";
-  java.style.left = "-150px";
-});
-five.addEventListener("click", () => {
-  python.style.left = "-150px";
-  js.style.left = "-150px";
-  html.style.left = "-150px";
-  css.style.left = "-150px";
-  java.style.left = "120px";
 });
